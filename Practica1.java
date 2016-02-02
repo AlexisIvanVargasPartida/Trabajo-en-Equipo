280aa0033c332d6b2675bc83ccacd9749ed0ebad
Juan <juraramirezga@ittepic.edu.mx>
2016-02-02 13:13:28 -0700

    agregando textfield y acomodando nombres

--- Practica1.java
+++ Practica1.java
@@ -1,13 +1,18 @@
 import javax.swing.*;
-public class Practica1 extends JFrame{
+public class Practica1 extends JFrame {
     private JLabel label1;
+    private JTextField textField;
     public Practica1() {
         setLayout(null);
-        label1=new JLabel("Alexis Ivan\nJuan Ramon\nEligardo\nDonadony");
-        label1.setBounds(20,30,300,40);
+        label1=new JLabel("Alexis Ivan \n Juan Ramon \n Eligardo \n Donadony");
+        label1.setBounds(20,30,600,40);
         add(label1);
+        textField = new JTextField(20);
+        textField.setText("Hola Mundo :)");
+        textField.setBounds(40,60,600,80);
+        add(textField);
     }
-    ////////////////////////////////////////////
+    
     public static void main(String[] ar) {
         Practica1 formulario1=new Practica1();// se crea el objeto de la clase
         formulario1.setBounds(20,20,300,400);
