0fc74d044f714da75324d288ebfb28f570d4d120
Alexis Ivan <alexivanvp2008hotmail.com>
2016-02-01 17:24:10 -0700

    Versión 0.1

---
+++ Practica1.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+public class Practica1 extends JFrame{
+    private JLabel label1;
+    public Practica1() {
+        setLayout(null);
+        label1=new JLabel("Alexis Ivan\nJuan Ramon\nEligardo\nDonadony");
+        label1.setBounds(20,30,300,40);
+        add(label1);
+    }
+    
+    public static void main(String[] ar) {
+        Practica1 formulario1=new Practica1();
+        formulario1.setBounds(20,20,300,400);
+        formulario1.setVisible(true);
+    }
+}
