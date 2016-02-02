21b8c54f617436a000788b97af7ae1ba8af778ea
Juan <juraramirezga@ittepic.edu.mx>
2016-02-01 17:51:21 -0700

    v2

--- Practica1.java
+++ Practica1.java
@@ -7,7 +7,7 @@ public class Practica1 extends JFrame{
         label1.setBounds(20,30,300,40);
         add(label1);
     }
-    
+    ////////////////////////////////////////////
     public static void main(String[] ar) {
         Practica1 formulario1=new Practica1();// se crea el objeto de la clase
         formulario1.setBounds(20,20,300,400);
